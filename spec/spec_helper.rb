c8c9cde7dd50238190a8979ac8f5e174c35ad461
Ben Holley <ben@isotope11.com>
2012-02-23 16:43:46 -0600

    add travis config and update spec_helper for tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,8 @@ require 'factory_girl'
 
   ENV["RAILS_ENV"] ||= 'test'
   require File.expand_path(File.dirname(__FILE__) + "/../fixture_rails_root/config/environment")
+  require Rails.root.join('db/migrate/20101203193439_create_plutus_tables.rb').to_s
+  CreatePlutusTables.up
   require 'rspec/rails'
 
   $: << File.expand_path(File.dirname(__FILE__) + '/../lib/')
