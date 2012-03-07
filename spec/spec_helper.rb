68b56dc6e2e8868d3919178e3a6e5e5f5eaf0865
Josh Adams <josh@isotope11.com>
2012-03-07 14:14:50 -0600

    Fix tests...

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,15 @@
 require 'factory_girl'
 
-  ENV["RAILS_ENV"] ||= 'test'
-  require File.expand_path(File.dirname(__FILE__) + "/../fixture_rails_root/config/environment")
-  require Rails.root.join('db/schema').to_s
-  require 'rspec/rails'
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path(File.dirname(__FILE__) + "/../fixture_rails_root/config/environment")
+require Rails.root.join('db/schema').to_s
+require 'rspec/rails'
 
-  $: << File.expand_path(File.dirname(__FILE__) + '/../lib/')
-  require 'plutus'
+$: << File.expand_path(File.dirname(__FILE__) + '/../lib/')
+require 'plutus'
 
-  Dir[File.expand_path(File.join(File.dirname(__FILE__),'factories','**','*.rb'))].each {|f| require f}
+Dir[File.expand_path(File.join(File.dirname(__FILE__),'factories','**','*.rb'))].each {|f| require f}
 
-  RSpec.configure do |config|
-    config.use_transactional_fixtures = true
-  end
+RSpec.configure do |config|
+  config.use_transactional_fixtures = true
+end
