638d3d2fd10752a9051f2a4325f20e39f5c52744
Michael Bulat <mbulat@gmail.com>
2012-03-07 17:55:00 -0500

    fix factory

--- spec/factories/transaction_factory.rb
+++ spec/factories/transaction_factory.rb
@@ -1,6 +1,8 @@
-FactoryGirl.define :transaction, :class => Plutus::Transaction do |transaction|
-  transaction.description 'factory description'
-  transaction.credit_account {|credit_account| credit_account.association(:asset)}
-  transaction.debit_account {|debit_account| debit_account.association(:revenue)}
-  transaction.amount BigDecimal.new('100')
+FactoryGirl.define do
+  factory :transaction, :class => Plutus::Transaction do |transaction|
+    transaction.description 'factory description'
+    transaction.credit_account {|credit_account| credit_account.association(:asset)}
+    transaction.debit_account {|debit_account| debit_account.association(:revenue)}
+    transaction.amount BigDecimal.new('100')
+  end
 end
