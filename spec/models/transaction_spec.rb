b7db6b61e22824b3b6e7a0daeab8a87d34d5caf0
Will Glynn <will@willglynn.com>
2013-02-17 22:07:21 -0600

    Simplify Plutus::Transaction spec

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -2,50 +2,50 @@ require 'spec_helper'
 
 module Plutus
   describe Transaction do
+    let(:transaction) { FactoryGirl.build(:transaction) }
+    subject { transaction }
 
-    it "should create a transaction" do
-      transaction = FactoryGirl.build(:transaction_with_credit_and_debit)
-      transaction.save!
+    it { should_not be_valid }
+
+    context "with credit and debit" do
+      let(:transaction) { FactoryGirl.build(:transaction_with_credit_and_debit) }
+      it { should be_valid }
+      
+      it "should require a description" do
+        transaction.description = nil
+        transaction.should_not be_valid
+      end
     end
 
-    it "should not be valid without a credit amount" do
-      transaction = FactoryGirl.build(:transaction)
-      transaction.debit_amounts << FactoryGirl.build(:debit_amount, :transaction => transaction)
-      transaction.should_not be_valid
-      transaction.errors['base'].should include("Transaction must have at least one credit amount")
+    context "with a debit" do
+      before {
+        transaction.debit_amounts << FactoryGirl.build(:debit_amount, transaction: transaction)
+      }
+      it { should_not be_valid }
+
+      context "with an invalid credit" do
+        before {
+          transaction.credit_amounts << FactoryGirl.build(:credit_amount, transaction: transaction, amount: nil)
+        }
+        it { should_not be_valid }
+      end
     end
 
-    it "should not be valid with an invalid credit amount" do
-      transaction = FactoryGirl.build(:transaction)
-      transaction.debit_amounts << FactoryGirl.build(:debit_amount, :transaction => transaction)
-      transaction.credit_amounts << FactoryGirl.build(:credit_amount, :transaction => transaction, :amount => nil)
-      transaction.should_not be_valid
-      transaction.errors[:credit_amounts].should == ["is invalid"]
-    end
+    context "with a credit" do
+      before {
+        transaction.credit_amounts << FactoryGirl.build(:credit_amount, transaction: transaction)
+      }
+      it { should_not be_valid }
 
-    it "should not be valid without a debit amount" do
-      transaction = FactoryGirl.build(:transaction)
-      transaction.credit_amounts << FactoryGirl.build(:credit_amount, :transaction => transaction)
-      transaction.should_not be_valid
-      transaction.errors['base'].should include("Transaction must have at least one debit amount")
-    end
-
-    it "should not be valid with an invalid debit amount" do
-      transaction = FactoryGirl.build(:transaction)
-      transaction.credit_amounts << FactoryGirl.build(:credit_amount, :transaction => transaction)
-      transaction.debit_amounts << FactoryGirl.build(:debit_amount, :transaction => transaction, :amount => nil)
-      transaction.should_not be_valid
-      transaction.errors[:debit_amounts].should == ["is invalid"]
-    end
-
-    it "should not be valid without a description" do
-      transaction = FactoryGirl.build(:transaction_with_credit_and_debit, :description => nil)
-      transaction.should_not be_valid
-      transaction.errors[:description].should == ["can't be blank"]
+      context "with an invalid debit" do
+        before {
+          transaction.debit_amounts << FactoryGirl.build(:debit_amount, transaction: transaction, amount: nil)
+        }
+        it { should_not be_valid }
+      end
     end
 
     it "should require the debit and credit amounts to cancel" do
-      transaction = FactoryGirl.build(:transaction)
       transaction.credit_amounts << FactoryGirl.build(:credit_amount, :amount => 100, :transaction => transaction)
       transaction.debit_amounts << FactoryGirl.build(:debit_amount, :amount => 200, :transaction => transaction)
       transaction.should_not be_valid
@@ -54,16 +54,16 @@ module Plutus
 
     it "should have a polymorphic commercial document associations" do
       mock_document = FactoryGirl.create(:asset) # one would never do this, but it allows us to not require a migration for the test
-      transaction = FactoryGirl.build(:transaction_with_credit_and_debit, :commercial_document => mock_document)
-      transaction.save
+      transaction = FactoryGirl.build(:transaction_with_credit_and_debit, commercial_document: mock_document)
+      transaction.save!
       saved_transaction = Transaction.find(transaction.id)
       saved_transaction.commercial_document.should == mock_document
     end
 
     it "should allow building a transaction and credit and debits with a hash" do
-      FactoryGirl.create(:asset, :name => "Accounts Receivable")
-      FactoryGirl.create(:revenue, :name => "Sales Revenue")
-      FactoryGirl.create(:liability, :name =>  "Sales Tax Payable")
+      FactoryGirl.create(:asset, name: "Accounts Receivable")
+      FactoryGirl.create(:revenue, name: "Sales Revenue")
+      FactoryGirl.create(:liability, name: "Sales Tax Payable")
       mock_document = FactoryGirl.create(:asset)
       transaction = Transaction.build(
         description: "Sold some widgets",
@@ -73,8 +73,8 @@ module Plutus
         credits: [
           {account: "Sales Revenue", amount: 45},
           {account: "Sales Tax Payable", amount: 5}])
-      transaction.should be_valid
-      transaction.save
+      transaction.save!
+
       saved_transaction = Transaction.find(transaction.id)
       saved_transaction.commercial_document.should == mock_document
     end
