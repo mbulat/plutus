b112ca124ba251ebccbf3c927573d55490fa1c63
Will Glynn <will@willglynn.com>
2013-02-17 22:02:22 -0600

    Refactor Plutus::Amounts to use shared example groups

--- spec/models/amount_spec.rb
+++ spec/models/amount_spec.rb
@@ -2,11 +2,29 @@ require 'spec_helper'
 
 module Plutus
   describe Amount do
+    subject { FactoryGirl.build(:amount) }
+    it { should_not be_valid }  # construct a child class instead
+  end
 
-    it "should not allow creating an amount without a subtype" do
-      amount = FactoryGirl.build(:amount)
+  shared_examples_for 'a Plutus::Amount subtype' do |elements|
+    let(:amount) { FactoryGirl.build(elements[:kind]) }
+    subject { amount }
+    
+    it { should be_valid }
+    
+    it "should require an amount" do
+      amount.amount = nil
       amount.should_not be_valid
     end
 
+    it "should require a transaction" do
+      amount.transaction = nil
+      amount.should_not be_valid
+    end
+    
+    it "should require an account" do
+      amount.account = nil
+      amount.should_not be_valid
+    end
   end
 end
