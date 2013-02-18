5167bd6507ecae9eb58ef93f66ef1a7b58eef308
Will Glynn <will@willglynn.com>
2013-02-17 21:57:50 -0600

    Refactor Plutus::Account children to use a shared example group

--- spec/models/equity_spec.rb
+++ spec/models/equity_spec.rb
@@ -2,53 +2,6 @@ require 'spec_helper'
 
 module Plutus
   describe Equity do
-
-    it "should allow creating an equity account" do
-      equity = FactoryGirl.create(:equity)
-    end
-
-    it "should report a balance for the equity account" do
-      equity = FactoryGirl.create(:equity)
-      FactoryGirl.create(:credit_amount, :account => equity)
-      equity.balance.should > 0
-      equity.balance.should be_kind_of(BigDecimal)
-    end
-
-    it "should report a balance for the class of accounts" do
-      equity = FactoryGirl.create(:equity)
-      FactoryGirl.create(:credit_amount, :account => equity)
-      Equity.should respond_to(:balance)
-      Equity.balance.should > 0
-      Equity.balance.should be_kind_of(BigDecimal)
-    end
-
-    it "should not report a trial balance" do
-      lambda{Equity.trial_balance}.should raise_error(NoMethodError)
-    end
-
-    it "should not be valid without a name" do
-      equity = FactoryGirl.build(:equity, :name => nil)
-      equity.should_not be_valid
-    end
-
-    it "should have many credit transactions" do
-      equity = Factory(:equity)
-      equity.should respond_to(:credit_transactions)
-    end
-
-    it "should have many debit transactions" do
-      equity = Factory(:equity)
-      equity.should respond_to(:debit_transactions)
-    end
-
-    it "a contra account should reverse the normal balance" do
-      equity = FactoryGirl.create(:equity)
-      contra_equity = Factory(:equity, :contra => true)
-      FactoryGirl.create(:credit_amount, :account => equity)
-      FactoryGirl.create(:debit_amount, :account => contra_equity)
-      contra_equity.balance.should > 0
-      Equity.balance.should == 0
-    end
-
+    it_behaves_like 'a Plutus::Account subtype', kind: :equity, normal_balance: :credit
   end
 end
--- spec/models/liability_spec.rb
+++ spec/models/liability_spec.rb
@@ -2,53 +2,6 @@ require 'spec_helper'
 
 module Plutus
   describe Liability do
-
-    it "should allow creating an liability account" do
-      liability = FactoryGirl.create(:liability)
-    end
-
-    it "should report a balance for the liability account" do
-      liability = FactoryGirl.create(:liability)
-      FactoryGirl.create(:credit_amount, :account => liability)
-      liability.balance.should > 0
-      liability.balance.should be_kind_of(BigDecimal)
-    end
-
-    it "should report a balance for the class of accounts" do
-      liability = FactoryGirl.create(:liability)
-      FactoryGirl.create(:credit_amount, :account => liability)
-      Liability.should respond_to(:balance)
-      Liability.balance.should > 0
-      Liability.balance.should be_kind_of(BigDecimal)
-    end
-
-    it "should not report a trial balance" do
-      lambda{Liability.trial_balance}.should raise_error(NoMethodError)
-    end
-
-    it "should not be valid without a name" do
-      liability = FactoryGirl.build(:liability, :name => nil)
-      liability.should_not be_valid
-    end
-
-    it "should have many credit transactions" do
-      liability = Factory(:liability)
-      liability.should respond_to(:credit_transactions)
-    end
-
-    it "should have many debit transactions" do
-      liability = Factory(:liability)
-      liability.should respond_to(:debit_transactions)
-    end
-
-    it "a contra account should reverse the normal balance" do
-      liability = FactoryGirl.create(:liability)
-      contra_liability = Factory(:liability, :contra => true)
-      FactoryGirl.create(:credit_amount, :account => liability)
-      FactoryGirl.create(:debit_amount, :account => contra_liability)
-      contra_liability.balance.should > 0
-      Liability.balance.should == 0
-    end
-
+    it_behaves_like 'a Plutus::Account subtype', kind: :liability, normal_balance: :credit
   end
 end
