1867eeb98dac215a0212eaebc4501b419734eb2b
Michael Bulat <mbulat@gmail.com>
2013-08-14 10:27:17 -0400

    rails 4 compatibllity updates

--- app/models/plutus/revenue.rb
+++ app/models/plutus/revenue.rb
@@ -39,7 +39,7 @@ module Plutus
     # @return [BigDecimal] The decimal value balance
     def self.balance
       accounts_balance = BigDecimal.new('0')
-      accounts = self.find(:all)
+      accounts = self.all
       accounts.each do |revenue|
         unless revenue.contra
           accounts_balance += revenue.balance
