2af42e4e7acf4c676370c5669aaeafe04504c9ed
Michael Bulat <mbulat@gmail.com>
2013-09-13 11:04:48 -0400

    ensure we require protected_attributes

--- Gemfile
+++ Gemfile
@@ -2,5 +2,3 @@ source "http://rubygems.org"
 
 # Specify your gem's dependencies in valkyrie.gemspec
 gemspec
-
-gem 'protected_attributes'
--- lib/plutus.rb
+++ lib/plutus.rb
@@ -1,5 +1,6 @@
 # Plutus
 require "rails"
+require "protected_attributes"
 module Plutus
   class Engine < Rails::Engine
     isolate_namespace Plutus
