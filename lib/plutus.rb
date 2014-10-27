73c97548a546a09d62856dca1833a5f060e7acdf
Yuva <yuva@codemancers.com>
2014-10-27 15:00:08 +0530

    [#issue-27] add attributes to configure tenancy for plutus

--- lib/plutus.rb
+++ lib/plutus.rb
@@ -5,4 +5,19 @@ module Plutus
   class Engine < Rails::Engine
     isolate_namespace Plutus
   end
+
+
+  # ------------------------------ tenancy ------------------------------
+  # configuration to enable or disable tenancy
+  mattr_accessor :enable_tenancy
+  enable_tenancy = true
+
+  mattr_accessor :tenant_class
+  tenant_class = nil
+
+
+  # provide hook to configure attributes
+  def self.config
+    yield(self)
+  end
 end
