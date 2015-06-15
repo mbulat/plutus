32a5236fd1d078c67a9bbf10ff647d989c2b227e
Ryan Hosford <tad.hosford@gmail.com>
2015-06-15 18:41:05 -0500

    Revives some routing specs
      - Draws the Plutus::Engine routes
      - Updates the specs to fix 2 errors (2 in each file)
      - Updates the routes to fix the remaining specs (only draw #show and #index routes)

--- spec/routing/accounts_routing_spec.rb
+++ spec/routing/accounts_routing_spec.rb
@@ -2,31 +2,32 @@ require 'spec_helper'
 
 module Plutus
   describe AccountsController do
-    # Run these tests if you enable routing in your rails app. See README 
-    #describe "routing" do
-      #it "recognizes and generates #index" do
-        #{ :get => "/accounts" }.should route_to(:controller => "accounts", :action => "index")
-      #end
+    routes { Plutus::Engine.routes }
 
-      #it "recognizes and generates #show" do
-        #{ :get => "/accounts/1" }.should route_to(:controller => "accounts", :action => "show", :id => "1")
-      #end
+    describe "routing" do
+      it "recognizes and generates #index" do
+        { :get => "/accounts" }.should route_to(:controller => "plutus/accounts", :action => "index")
+      end
 
-      #it "recognizes and generates #edit" do
-        #{ :get => "/accounts/1/edit" }.should_not be_routable
-      #end
+      it "recognizes and generates #show" do
+        { :get => "/accounts/1" }.should route_to(:controller => "plutus/accounts", :action => "show", :id => "1")
+      end
 
-      #it "recognizes and generates #create" do
-        #{ :post => "/accounts" }.should_not be_routable
-      #end
+      it "recognizes and generates #edit" do
+        { :get => "/accounts/1/edit" }.should_not be_routable
+      end
 
-      #it "recognizes and generates #update" do
-        #{ :put => "/accounts/1" }.should_not be_routable
-      #end
+      it "recognizes and generates #create" do
+        { :post => "/accounts" }.should_not be_routable
+      end
 
-      #it "recognizes and generates #destroy" do
-        #{ :delete => "/accounts/1" }.should_not be_routable
-      #end
-    #end
+      it "recognizes and generates #update" do
+        { :put => "/accounts/1" }.should_not be_routable
+      end
+
+      it "recognizes and generates #destroy" do
+        { :delete => "/accounts/1" }.should_not be_routable
+      end
+    end
   end
 end
