e5572f5e4b1fb84304f2185795d62adf818c28fb
Michael Bulat <mbulat@gmail.com>
2015-07-28 16:43:11 -0400

    simple income statement

--- app/controllers/plutus/reports_controller.rb
+++ app/controllers/plutus/reports_controller.rb
@@ -22,5 +22,18 @@ module Plutus
       end
     end
 
+    # @example
+    #   GET /reports/income_statement
+    def income_statement
+      @from_date = params[:from_date] ? Date.parse(params[:from_date]) : Date.today.at_beginning_of_month
+      @to_date = params[:to_date] ? Date.parse(params[:to_date]) : Date.today
+      @revenues = Plutus::Revenue.all
+      @expenses = Plutus::Expense.all
+
+      respond_to do |format|
+        format.html # index.html.erb
+      end
+    end
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Plutus::Engine.routes.draw do
   root :to => 'reports#balance_sheet'
 
   get 'reports/balance_sheet' => 'reports#balance_sheet'
+  get 'reports/income_statement' => 'reports#income_statement'
 
   resources :accounts, only: [:index]
   resources :entries, only: [:index]
