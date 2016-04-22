f5ebafa4d941cbe0b5c861a8cfc1330db0453c45
Ramon Tayag <ramon.tayag@gmail.com>
2016-04-22 09:10:15 +0800

    Use standard way of generating migrations in engines
    Keep tenant column:
    
    Until we merge the feature that always turns on tenancy
    (https://github.com/mbulat/plutus/pull/79), we must keep
    the dummy Rails app's migrations disjoint from what
    `rake plutus:migrations:install` actually copies over

--- fixture_rails_root/db/schema.rb
+++ fixture_rails_root/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170710174915) do
+ActiveRecord::Schema.define(version: 20180709082453) do
 
   create_table "plutus_accounts", force: :cascade do |t|
     t.string "name"
