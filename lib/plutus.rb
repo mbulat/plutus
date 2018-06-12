4774c0d8fd3e95b9aa99147ceb8ebdca87f1048f
Victor Morandini Stabile <vmstabile@gmail.com>
2018-06-12 11:27:58 -0300

    decorators support

--- lib/plutus.rb
+++ lib/plutus.rb
@@ -7,6 +7,12 @@ require "kaminari"
 module Plutus
   class Engine < Rails::Engine
     isolate_namespace Plutus
+
+    config.to_prepare do
+      Dir.glob(Rails.root + "app/decorators/**/*_decorator*.rb").each do |c|
+        require_dependency(c)
+      end
+    end
   end
 
 
