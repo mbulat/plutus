e5aa29bf9ea295b2c0428535b494efa2b918eddc
Jorge Daniel Sampayo Vargas <jdsampayo@gmail.com>
2020-04-06 11:21:19 -0500

    Remove BigDecimal new for compatibility with Ruby 2.7.1

--- spec/factories/amount_factory.rb
+++ spec/factories/amount_factory.rb
@@ -1,18 +1,18 @@
 FactoryGirl.define do
   factory :amount, :class => Plutus::Amount do |amount|
-    amount.amount BigDecimal.new('473')
+    amount.amount BigDecimal('473')
     amount.association :entry, :factory => :entry_with_credit_and_debit
     amount.association :account, :factory => :asset
   end
 
   factory :credit_amount, :class => Plutus::CreditAmount do |credit_amount|
-    credit_amount.amount BigDecimal.new('473')
+    credit_amount.amount BigDecimal('473')
     credit_amount.association :entry, :factory => :entry_with_credit_and_debit
     credit_amount.association :account, :factory => :revenue
   end
 
   factory :debit_amount, :class => Plutus::DebitAmount do |debit_amount|
-    debit_amount.amount BigDecimal.new('473')
+    debit_amount.amount BigDecimal('473')
     debit_amount.association :entry, :factory => :entry_with_credit_and_debit
     debit_amount.association :account, :factory => :asset
   end
