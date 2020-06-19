bc18525180af5ae0a043500112a7661d37ac90e3
Michael Bulat <mbulat@gmail.com>
2020-06-19 15:06:07 -0400

    update bigdecimal for new ruby, and fix old factoryfirl syntax

--- spec/factories/entry_factory.rb
+++ spec/factories/entry_factory.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :entry, :class => Plutus::Entry do |entry|
-    entry.description 'factory description'
+    entry.description { 'factory description' }
     factory :entry_with_credit_and_debit, :class => Plutus::Entry do |entry_cd|
       entry_cd.after_build do |t|
         t.credit_amounts << FactoryGirl.build(:credit_amount, :entry => t)
